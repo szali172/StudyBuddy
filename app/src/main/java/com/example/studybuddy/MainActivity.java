55f631f8889b7c20144b16c1d25c535f5dda24ff
Viven Puthenpurayil <viven@Vivens-MacBook-Pro.local>
2022-10-05 17:38:47 -0500

    App can get information from database

--- app/src/main/java/com/example/studybuddy/MainActivity.java
+++ app/src/main/java/com/example/studybuddy/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.studybuddy;
 
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView pagenameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pagenameTextView = findViewById(R.id.homepage);
+
+        // OkHttpClient client created
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        // Get request for John Smith
+        Request request = new Request.Builder().url("http://10.0.2.2:5000/buddies/Users/name/John%20Smith").build();
+
+        // asynchronous call
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            // if there is a failure when reaching the server
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "server not available", Toast.LENGTH_SHORT).show();
+                        pagenameTextView.setText("error connecting to the server");
+                    }
+                });
+            }
+
+            @Override
+            // executed if we get an actual response from the server
+            public void onResponse(
+                    @NotNull Call call,
+                    @NotNull Response response)
+                    throws IOException {pagenameTextView.setText(response.body().string());
+            }
+        });
     }
 }
--- server.py
+++ server.py
@@ -7,30 +7,45 @@ from pymongo import MongoClient
 cluster = MongoClient(f'mongodb+srv://{c.username}:{c.pw}@studdybuddy.ptwaiia.mongodb.net/?retryWrites=true&w=majority')
 
 
-@app.route('/<db>/<coll>/<entry>', methods=['PUT', 'GET', 'DELETE'])
-def get_name(db, coll, entry):
+"""
+Find a person entry in the database given a key (i.e. 'name', 'email', etc.) and a value ('John Smith')
+"""
+@app.route('/<db>/<coll>/<key>/<value>', methods=['PUT', 'GET', 'DELETE'])
+def get_person(db, coll, key, value):
     
     # Check to see if db and collection exist
     try:
-        database = cluster[db] # archives
-        collection = database[coll] # tuscola, 404, etc.
+        database = cluster[db] 
+        collection = database[coll]
     except:
         return f'Could not access {db} or {coll}\n', 400
+    
+    # Get request
     if request.method == 'GET':    
-        print("hi")
-        cursor = collection.find_one({'name': 'John Smith'})
+        cursor = collection.find_one({key: value})
         
+        # Returns the persons email
         if cursor:
-            print(cursor['email'])
-        return 'GET worked', 200
+            return cursor["email"], 200
+    # Put request
     elif request.method == 'PUT':     
-        print("hey")
+        # def put_key(key):
+        # value = request.data.decode("utf-8") # content ex) date
+        # version_num = db.key.count_documents({'key' : key})
+        # db.key.insert_one({'version' : version_num+1, 'value' : value, 'key' : key})
+        # return 'HTTP/', 200
+
         return 'PUT worked', 200
+
+    # delete request
     elif request.method == 'DELETE':  
-        print("heyyyyyyy")
+        # try:
+        #     db.key.delete_many({'key' : key})
+        #     return 'HTTP/', 200
+        # except: 
+        #     return 'Key does not exist', 400
         return 'DELETE worked', 200
     else:
         return 'Error', 405
     
-    
-    # curl -X GET http://localhost:5000/buddies/Users/name
+# curl -X GET http://localhost:5000/buddies/Users/name/John%20Smith
