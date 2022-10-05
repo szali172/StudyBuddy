91165a105ee6f4e8d0823f8d2715aa70a52f4c3b
sun-div <divsundar9@gmail.com>
2022-10-05 13:48:49 -0500

    fixed java case style, array->arrayList, started filter

---
+++ app/src/main/java/com/example/studybuddy/FilterSearch.java
@@ -0,0 +1,11 @@
+package com.example.studybuddy;
+import java.util.ArrayList;
+public class Filter extends Profile {
+
+    private ArrayList<Course> displayedCourses;
+
+    public void filterByAlpha() {}
+    public void filterByPrefix(String prefix) {}
+    public void addToFavorites() {}
+    public void filterByName(String search) {}
+}
