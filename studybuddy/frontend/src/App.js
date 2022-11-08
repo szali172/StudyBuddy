2d8acec5bce5d6b3d4c1d05f902de3ed7d52a6f2
Viven Puthenpurayil <viven@Vivens-MacBook-Pro.local>
2022-11-08 16:57:21 -0600

    updating server.py

--- studybuddy/frontend/src/App.js
+++ studybuddy/frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import axios from "axios";
-import logo from './logo.svg';
+import axios from "axios"
+import logo from './logo.svg'
 import './App.css';
 
 function App() {
